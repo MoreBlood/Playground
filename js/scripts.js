cb170b95f5aad0cf67eefe85aafb045db19db4eb
Kirill <my-email@com.com>
2018-01-11 18:33:36 +0300

    убрал немного

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,3 @@ $(document).ready(function() {
         getPage(this.hash);
     });
 });
-
-function getPage(pageName) {
-    $.ajax({
-        type: 'GET',
-        url: `pages/${pageName.replace('#', '')}.html`,
-        dataType: "html",   //expect html to be returned
-        success: function(msg) {
-            if(parseInt(msg) !== 0)   
-            {
-                $('article').html(msg);    
-            }
-        }
-    });
-};
