84c256f1eb18dcbd0a4378a9822015ef20620bea
Kirill Artihovich <new.shtirlic@mail.ru>
2018-01-20 03:02:27 +0300

    router changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,44 @@
-if (document.location.hash !== "/") {
-    getPage(document.location.hash)
-}
-
-$(document).ready(function() {
-    $('header a').click(function (e) {
-        e.preventDefault()
-        var path = $(this).attr('href')
-        window.history.replaceState({}, path, path);
-        getPage(path);
-    });
+$(document).ready(() => {
+  if (document.location.hash !== '/') {
+    getPage(document.location.hash);
+  }
+  $('header a').click(function (e) {
+    e.preventDefault();
+    const path = $(this).attr('href');
+    window.history.replaceState({}, path, path);
+    getPage(path);
+  });
 });
 
+const print = (data) => {
+  console.log(data);
+};
 
-function scrollToId (id) {
-    $('html, body').animate({
-        scrollTop:  $(id).offset().top - 10
-    }, 500);
+function scrollToId(id) {
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - 10,
+  }, 500);
 }
 
-function getPage(pageName) {
-    if (pageName === "") {
-        pageName = "index"
-    }
-    $.ajax({
-        type: 'GET',
-        url: `pages/${pageName.replace('#/', '')}.html`,
-        dataType: "html",   //expect html to be returned
-        success: function(msg) {
-            $('article').html(msg);
-            if ((/\?/).test(pageName)) {
-                scrollToId("#" + pageName.substring(pageName.indexOf('?') + 1, pageName.length));
-            }  
-        },
-        error: function(erorr) {
-            $('article').html(`<h2>404</h2> <h3>${erorr.statusText}</h3>`); 
-        },
-    });
-};
+function getPage(page) {
+  let pageName = page;
+
+  if (!pageName) {
+    pageName = 'index';
+  }
+
+  $.ajax({
+    type: 'GET',
+    url: `pages/${pageName.replace('#/', '').substring(0, pageName.indexOf('/#') == -1 ? pageName.length : pageName.indexOf('/#') - 2)}.html`,
+    dataType: 'html',
+    success(msg) {
+      $('article').html(msg);
+      if ((/\/\#/).test(pageName)) {
+        scrollToId(`${pageName.substring(pageName.indexOf('/#') + 1, pageName.length)}`);
+      }
+    },
+    error(error) {
+      $('article').html(`<h1>${error.status}</h1> <h2>${error.statusText}</h2>`);
+    },
+  });
+}
